92a07b95c4329fdaf0eb9e25804ce769c8c8d08a
mouctare <mouctard78@gmail.com>
2020-06-18 16:30:05 +0200

    Ajout des commentaires sur le planningSubscriber

---
+++ src/Events/PanningUserSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\Planning;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+
+
+
+class PlanningUserSubscriber implements EventSubscriberInterface 
+{
+    private $security;
+
+    public function __construct(Security $security) 
+    {
+        $this->security = $security;
+    }
+  // Toute classe qui implements le EventSubscriberInterface se doit d'avoir la methode =>
+  public static function getSubscribedEvents() 
+  {
+    // Cette function renvoit un tableau auquel on va souscrire à des événments
+    return [
+    // La clée du tableau est une constante qui se trouve dans la class kernelEvents qui s'appelle view
+    // Au moment du view, on va avoir une méthode qui s'appelle setUserForPlanning et qui devra etre appellé sur une priorité
+    // Et pour avoir la priorité il faut applé la class EventPriorities et applé la constant PRE_VALIDATE
+          KernelEvents::VIEW => ['setUserForPlanning', EventPriorities::PRE_VALIDATE]
+      
+    ];
+   
+  }
+
+   // On va crée la méthode setUserForPlanning
+   // Quand on est branché un evenement view il faut avoir une instance de EiewEvent
+   public function setUserForPlanning(ViewEvent $event) {
+       $planning = $event->getControllerResult();
+       $method = $event->getRequest()->Method();
+
+       if($planning instanceof Planning && $method === "POST") {
+           $user = $this->security->getUser();
+           $planning->setUser($user);
+
+
+       }
+   }
+}
